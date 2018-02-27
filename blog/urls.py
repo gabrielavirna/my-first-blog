eccefeddb4038b53d33949df7f2bdc9dd2626798
Gabriela Mihaila <gabrielavirna012@gmail.com>
2018-02-26 16:44:07 -0800

    Added views to create/edit blog post inside the site.

--- blog/urls.py
+++ blog/urls.py
@@ -3,8 +3,9 @@ from . import views
 
 # we're importing Django's function url and all of our views from the blog application.
 # add our first URL pattern; we're now assigning a view called post_list to the ^$ URL
+
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
-    url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail')
+    url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
+    url(r'^post/new/$', views.post_new, name='post_new'),
 ]
-
